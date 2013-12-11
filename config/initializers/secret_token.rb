07883fe8774eb5fd9ee16030579aa2863129df9f
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2013-12-11 14:14:26 -0500

    Initial commit with rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nevernote::Application.config.secret_token = '0ca271f31430ba51ba24a9cde590f89aa1dc92bced307f67497474c62200b3c1cf88a4a1bf4f7f86ed368a6feaac58246e7555586da52ba3de164eb0ecb10677'
