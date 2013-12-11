0d1cdbe42e78ec73fedc8afb3df3ff5359899ae1
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2013-12-11 15:46:48 -0500

    Add User methods to set and check passwords
    Uses BCrypt to hash and salt the password.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,20 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :username, :password
+  attr_reader :password
 
   validates :email, :username, :session_token, presence: true
   validates :password, length: { within: 6..64, allow_nil: true }
   validates :password_digest, presence: { message: "Password can't be blank" }
   validates :email, email: true
   validates :username, username: true
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
 end
