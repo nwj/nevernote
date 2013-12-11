a1bc6689f274d785a78d4de9b7b9c42713e94a36
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2013-12-11 17:19:31 -0500

    Build basic user sign-up form & controller routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+  def new
+    render :new
+  end
+
+  def create
+    user = User.new(params[:user])
+
+    if user.save
+      redirect_to user_url(user)
+    else
+      render json: user.errors.full_messages
+    end
+  end
+
+  # TEMPORARY - Final app will not have a users/show route
+  def show
+    render :show
+  end
+
 end
