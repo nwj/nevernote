140be18f01eb937864402b1f32f148ee68aaacb2
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2013-12-11 15:03:37 -0500

    Setup users database table and migrate database

--- db/migrate/20131211195451_create_users.rb
+++ db/migrate/20131211195451_create_users.rb
@@ -1,8 +1,17 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
+      t.string :username, null: false
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+      t.integer :notebook_id
       t.timestamps
     end
+
+    add_index :users, :username, unique: true
+    add_index :users, :email, unique: true
+    add_index :users, :session_token, unique: true
+    add_index :users, :notebook_id
   end
 end
