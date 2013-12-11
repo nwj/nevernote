e49682e0d65a9ffcefacd7dc91940d23d0adf950
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2013-12-11 15:53:21 -0500

    Move user helper methods to private

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,13 +19,15 @@ class User < ActiveRecord::Base
     BCrypt::Password.new(self.password_digest).is_password?(password)
   end
 
-  def self.generate_session_token
-    SecureRandom::urlsafe_base64(16)
-  end
-
   def reset_session_token!
     self.session_token = self.class.generate_session_token
     self.save!
+  end
+
+  private
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
   end
 
   def ensure_session_token
