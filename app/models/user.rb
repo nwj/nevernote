05245dcf39578b8bbf4ca252f373fb22ca0643db
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2013-12-11 15:42:55 -0500

    Add validations for Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :email, :username, :password
+
+  validates :email, :username, :session_token, presence: true
+  validates :password, length: { within: 6..64, allow_nil: true }
+  validates :password_digest, presence: { message: "Password can't be blank" }
+  validates :email, email: true
+  validates :username, username: true
 end
