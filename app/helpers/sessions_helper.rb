9a761f214d1c59992222ab8b4eafad85f75eb0e1
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2013-12-11 16:42:28 -0500

    Add helper methods for logging users in and out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,23 @@
 module SessionsHelper
+  def current_user
+    User.find_by_session_token(session[:session_token]
+  end
+
+  def logout!(user)
+    user.reset_session_token!
+    session[:session_token] = nil
+  end
+
+  def login!(user)
+    user.reset_session_token!
+    session[:session_token] = user.session_token
+  end
+
+  def require_logged_in
+    redirect_to new_session_url if current_user.nil?
+  end
+
+  def require_logged_out
+    redirect_to home_url unless current_user.nil?
+  end
 end
