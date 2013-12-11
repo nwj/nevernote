29643ba1cc18a652af33c3a55d9893cc8402e0a1
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2013-12-11 17:47:58 -0500

    Add basic sign in form & session controller routes
    This also required the inclusion of the session helper in the
    application controller, which should have been done earlier anyway.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
+
   protect_from_forgery
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,25 @@
 class SessionsController < ApplicationController
+
+  def new
+    render :new
+  end
+
+  def create
+    email_or_username = params[:email_or_username ]
+    password = params[:password]
+    user = User.find_by_credentials(email_or_username , password)
+
+    if user.nil?
+      render json: "Incorrect username and/or password."
+    else
+      login!(user)
+      redirect_to user_url(user)
+    end
+  end
+
+  def destroy
+    logout!(current_user)
+    redirect_to root_url
+  end
+
 end
