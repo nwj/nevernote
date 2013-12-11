5920f9e8704fc376a94a97260bc6e17b820110f4
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2013-12-11 16:59:40 -0500

    Fix typo in SessionsHelper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,6 @@
 module SessionsHelper
   def current_user
-    User.find_by_session_token(session[:session_token]
+    User.find_by_session_token(session[:session_token])
   end
 
   def logout!(user)
