2b96166c1106913b9ebf8417f88efacd8c9209ee
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2013-12-11 14:54:32 -0500

    Install and configure rspec

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,18 @@ module Nevernote
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Set which spec files to generate
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: true
+
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
   end
 end
