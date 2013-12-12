fec715e15f0ee53a3611e184fa58b2065cad1d31
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2013-12-12 13:25:09 -0500

    Add form to create or update a notebook
    This does not yet include functionality for changing the user's default
    notebook.

--- app/controllers/notebooks_controller.rb
+++ app/controllers/notebooks_controller.rb
@@ -5,4 +5,35 @@ class NotebooksController < ApplicationController
     render :index
   end
 
+  def new
+    @notebook = Notebook.new
+    render :new
+  end
+
+  def create
+    notebook = Notebook.new(params[:notebook])
+    notebook.user_id = current_user.id
+
+    if notebook.save
+      redirect_to notebooks_url
+    else
+      render json: notebook.errors.full_messages
+    end
+  end
+
+  def edit
+    @notebook = Notebook.find(params[:id])
+    render :edit
+  end
+
+  def update
+    notebook = Notebook.find(params[:id])
+
+    if notebook.update_attributes(params[:notebook])
+      redirect_to notebooks_url
+    else
+      render json: notebook.errors.full_messages
+    end
+  end
+
 end
