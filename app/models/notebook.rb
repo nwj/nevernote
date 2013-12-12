ea76c06ff8fad0ba9f019f9af4d12fac185ec38b
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2013-12-12 09:36:30 -0500

    Add validations for Notebooks

--- app/models/notebook.rb
+++ app/models/notebook.rb
@@ -1,3 +1,6 @@
 class Notebook < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :name, :user_id
+
+  validates :name, :user_id, presence: true
+  validates :name, uniqueness: { scope: :user_id }
 end
