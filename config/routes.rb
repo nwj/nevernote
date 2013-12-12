1d7b474b2caa3de15933d2c9943cc879c9d9b2a2
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2013-12-12 11:18:47 -0500

    Add routes for notebook and notes
    Also eliminated Users#show as it will be replaced by one of these new
    routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Nevernote::Application.routes.draw do
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create]
   resource :session, only: [:new, :create, :destroy]
+
+  resources :notebooks
+  resources :notes
 
   root to: "landing#show"
 end
