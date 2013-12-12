77d4e34d3feed0021b932743a0e07e1823e02a5e
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2013-12-12 15:48:45 -0500

    Enable note creation
    Notes are always instantiated as untitled and otherwise empty. The user
    can then edit them later.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -13,4 +13,17 @@ class NotesController < ApplicationController
 
     render :index
   end
+
+  def create
+    notebook = Notebook.find(params[:notebook_id]) if params[:notebook_id]
+
+    if notebook && notebook.author == current_user
+      Note.create(notebook_id: notebook.id)
+      redirect_to notes_url(notebook_id: notebook.id)
+    else
+      Note.create(notebook_id: current_user.default_notebook.id)
+      redirect_to notes_url
+    end
+
+  end
 end
