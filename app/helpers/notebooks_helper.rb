abc3615c31af09af481699caf3c4d56f7cde3a6e
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2013-12-13 17:58:21 -0500

    Add helpers to get note index data formatted nice

--- app/helpers/notebooks_helper.rb
+++ app/helpers/notebooks_helper.rb
@@ -6,4 +6,21 @@ module NotebooksHelper
   def change_default_notebook!(user, notebook)
     user.update_attributes(notebook_id: notebook.id)
   end
+
+  def time_since_last_update(note)
+    time_string = "#{time_ago_in_words(note.updated_at)} ago"
+  end
+
+  def generate_snippet(note)
+    return "" if note.content.nil?
+
+    words = note.content.split(' ')
+    snippet = String.new
+
+    until snippet.length > 200 || words.empty?
+      snippet += "#{words.shift} "
+    end
+
+    snippet
+  end
 end
--- app/views/api/notebooks/show.rabl
+++ app/views/api/notebooks/show.rabl
@@ -1,11 +1,11 @@
 object @notebook
 attributes :id, :name
 child(@notes) do
-  attributes :id, :title, :updated_at
-  node :updated_ago do |n|
-    "#{time_ago_in_words(n.updated_at)} ago"
+  attributes :id, :title
+  node :last_updated do |n|
+    time_since_last_update(n)
   end
   node :snippet do |n|
-    n.content[0..190] unless n.content.nil?
+    generate_snippet(n)
   end
 end
