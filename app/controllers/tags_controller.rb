fa4b89cd9ed0bd813ccf21ca2f979bb2b773ab55
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2013-12-13 11:25:19 -0500

    Define all tags controller actions

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,49 @@
 class TagsController < ApplicationController
+
+  def index
+    @tags = current_user.tags
+    render :index
+  end
+
+  def show
+    @tag = Tag.find(params[:id])
+    @notes = @tag.notes
+  end
+
+  def new
+    @tag = Tag.new()
+    render :new
+  end
+
+  def create
+    tag = Tag.new(params[:tag])
+    tag.user_id = current_user.id
+
+    if tag.save
+      redirect_to tags_url
+    else
+      render json: tag.errors.full_messages
+    end
+  end
+
+  def edit
+    @tag = Tag.find(params[:id])
+    render :edit
+  end
+
+  def update
+    tag = Tag.find(params[:id])
+
+    if tag.update_attributes(params[:tag])
+      redirect_to tags_url
+    else
+      render json: tag.errors.full_messages
+    end
+  end
+
+  def destroy
+    tag = Tag.find(params[:id])
+    tag.destroy
+    redirect_to tags_url
+  end
 end
