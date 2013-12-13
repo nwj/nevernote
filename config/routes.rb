9cb40f72d793c4094054de3b228ac779824de750
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2013-12-13 15:02:30 -0500

    Namespace api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,25 @@
 Nevernote::Application.routes.draw do
+  # Plain rails routes. For testing purposes only.
   resources :users, only: [:new, :create]
   resource :session, only: [:new, :create, :destroy]
-
   resources :notebooks do
-    member do
-      get 'rename'
-    end
+    get 'rename', on: :member
   end
-
+  resources :tags
   resources :notes, except: [:new] do
     resources :taggings, only: [:create, :destroy]
   end
 
-  resources :tags
+  # API routes. Backbone plugs in here.
+  namespace 'api', defaults: {format: :json} do
+    resources :notebooks, except: [:new, :edit]
+    resources :tags, except: [:new, :edit]
+    resources :notes, except: [:new, :edit] do
+      resources :taggings, only: [:create, :destroy]
+    end
+  end
 
-
+  # Interior and exterior roots
   resource :home, only: [:show]
-
   root to: "landing#show"
 end
