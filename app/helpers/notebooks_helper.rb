3fe99c7e259accec4a3b3ad0e0cdbac66e4b7d8c
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2013-12-14 18:13:27 -0500

    Stringify time since last note update properly
    Evernote uses a convention here that is not the same as rails's
    time_ago_in_words method

--- app/helpers/notebooks_helper.rb
+++ app/helpers/notebooks_helper.rb
@@ -8,7 +8,25 @@ module NotebooksHelper
   end
 
   def time_since_last_update(note)
-    time_string = "#{time_ago_in_words(note.updated_at)} ago"
+    time = note.updated_at
+
+    if time > 2.seconds.ago
+      "1 second ago"
+    elsif time > 1.minute.ago
+      "#{(Time.now - time).to_i} seconds ago"
+    elsif time > 2.minutes.ago
+      "1 minute ago"
+    elsif time > 1.hour.ago
+      "#{((Time.now - time) / 60).to_i} minutes ago"
+    elsif time > 1.days.ago
+      "#{time_ago_in_words(time)[6..-1]} ago"
+    elsif time > 2.days.ago
+      "Yesterday"
+    elsif time > 1.week.ago
+      "#{time_ago_in_words(time)} ago"
+    else
+      time.to_date.to_s
+    end
   end
 
   def generate_snippet(note)
