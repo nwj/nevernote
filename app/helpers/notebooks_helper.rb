36238acf9e0c748f68c619494651f32b50a46d90
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2013-12-14 21:11:17 -0500

    Clean up notebooks helper
    Forgot to take methods out of this helper when they were moved to the
    notes helper a few commits ago.

--- app/helpers/notebooks_helper.rb
+++ app/helpers/notebooks_helper.rb
@@ -7,38 +7,4 @@ module NotebooksHelper
     user.update_attributes(notebook_id: notebook.id)
   end
 
-  def time_since_last_update(note)
-    time = note.updated_at
-
-    if time > 2.seconds.ago
-      "1 second ago"
-    elsif time > 1.minute.ago
-      "#{(Time.now - time).to_i} seconds ago"
-    elsif time > 2.minutes.ago
-      "1 minute ago"
-    elsif time > 1.hour.ago
-      "#{((Time.now - time) / 60).to_i} minutes ago"
-    elsif time > 1.days.ago
-      "#{time_ago_in_words(time)[6..-1]} ago"
-    elsif time > 2.days.ago
-      "Yesterday"
-    elsif time > 1.week.ago
-      "#{time_ago_in_words(time)} ago"
-    else
-      time.strftime("%-m/%d/%y")
-    end
-  end
-
-  def generate_snippet(note)
-    return "" if note.content.nil?
-
-    words = note.content.split(' ')
-    snippet = String.new
-
-    until snippet.length > 200 || words.empty?
-      snippet += "#{words.shift} "
-    end
-
-    snippet
-  end
 end
