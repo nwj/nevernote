fc74df3e2a011515e2358bd3cb698ef89b55031e
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2013-12-15 12:42:44 -0500

    Add uniqueness constraint to taggings model
    So that the same note cannot be related multiple times to the same tag.

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -2,6 +2,7 @@ class Tagging < ActiveRecord::Base
   attr_accessible :note_id, :tag_id
 
   validates :note_id, :tag_id, presence: true
+  validates :note_id, uniqueness: { scope: :tag_id }
 
   belongs_to(
     :note,
