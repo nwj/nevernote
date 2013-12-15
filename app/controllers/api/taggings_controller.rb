e6267ba1ba4fe10ac069b57321398c88d5c7e410
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2013-12-15 12:40:47 -0500

    Define api/taggings controller actions

--- app/controllers/api/taggings_controller.rb
+++ app/controllers/api/taggings_controller.rb
@@ -1,2 +1,30 @@
 class Api::TaggingsController < ApplicationController
+
+  before_filter { |c| c.require_ownership("Note", params[:note_id]) }
+  before_filter(except: [:create]) { |c| c.require_ownership("Tagging") }
+
+  def create
+    @note = Note.find(params[:note_id])
+
+    @tag = Tag.find_or_create_by_name_and_user_id(
+      params[:tag_name],
+      current_user.id
+    )
+
+    @tagging = Tagging.create(note_id: @note.id, tag_id: @tag.id)
+
+    if @tagging.save
+      render "api/taggings/show"
+    else
+      render json: @tagging.errors.full_messages
+    end
+  end
+
+  def destroy
+    @tagging = Tagging.find(params[:id])
+
+    @tagging.destroy
+
+    render json: "Note untagged."
+  end
 end
---
+++ app/views/api/taggings/show.rabl
@@ -0,0 +1,5 @@
+object @tagging
+attributes :id
+node :tag_name do |t|
+  @tagging.tag.name
+end
