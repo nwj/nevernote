fb13230f4d1ffec72bdd934e17cc6ac7c5619245
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2013-12-17 12:48:22 -0500

    Bootstrap initial data set into backbone

--- app/assets/javascripts/nevernote.js
+++ app/assets/javascripts/nevernote.js
@@ -3,7 +3,14 @@ window.Nevernote = {
   Collections: {},
   Views: {},
   Routers: {},
-  initialize: function() {
-    console.log('Hello from Backbone!');
+  initialize: function(data) {
+    this.notebooks = new Nevernote.Collections.Notebooks(data.notebooks);
+    this.tags = new Nevernote.Collections.Tags(data.tags);
+
+    new Nevernote.Routers.Home({ notebooks: this.notebooks, tags: this.tags });
+    if (!Backbone.history.started) {
+      Backbone.history.start();
+      Backbone.history.started = true;
+    }
   }
 };
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,21 @@
 class HomesController < ApplicationController
   def show
+    notebooks = current_user.notebooks.order('name')
+    @notebooks_json = Rabl.render(
+      notebooks,
+      'index',
+      view_path: 'app/views/api/notebooks',
+      format: :json
+    )
+
+    tags = current_user.tags.order('name')
+    @tags_json = Rabl.render(
+      tags,
+      'index',
+      view_path: 'app/views/api/tags',
+      format: :json
+    )
+
     render :show
   end
 end
