a9b04bc86d642baf2624d726b32b34224ded8e65
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2013-12-17 14:55:15 -0500

    Render notebooks index
    This is pretty hacky right now, but I wanted to get something on the
    page as a starting point. Long run goal is to refactor this into
    something that uses composite views and therefore doesn't require me to
    overwrite backbone-support's swap function.

--- app/assets/javascripts/routers/home_router.js
+++ app/assets/javascripts/routers/home_router.js
@@ -1,3 +1,28 @@
-Nevernote.Routers.Home = Backbone.Router.extend({
+Nevernote.Routers.Home = Support.SwappingRouter.extend({
+  initialize: function(options) {
+    this.rootEl = $('.content');
+    this.notebooks = options.notebooks;
+    this.tags = options.tags;
+  },
+
+  routes: {
+    "" : "all",
+  },
+
+  all: function(){
+    var notebooksView = new Nevernote.Views.NotebooksIndex(
+      { notebooks: this.notebooks }
+    );
+    this.swap(notebooksView, $('.notebooks'));
+  },
+
+  swap: function(newView, el) {
+    if (this.currentView && this.currentView.leave) {
+      this.currentView.leave();
+    }
+
+    this.currentView = newView;
+    $(el).empty().append(this.currentView.render().el);
+  }
 
 });
--- app/assets/javascripts/views/notebooks/notebooks_index.js
+++ app/assets/javascripts/views/notebooks/notebooks_index.js
@@ -1,5 +1,20 @@
 Nevernote.Views.NotebooksIndex = Backbone.View.extend({
 
-  template: JST['notebooks/index']
+  template: JST['notebooks/index'],
+
+  initialize: function(options){
+    this.notebooks = options.notebooks;
+  },
+
+  render: function() {
+    console.log(this.notebooks)
+    this.$el.html(this.template({ notebooks: this.notebooks }));
+
+    return this;
+  },
+
+  leave: function(){
+    this.remove();
+  }
 
 });
