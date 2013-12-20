e8fdefc2b5a3b19a771a58d4ef8d55c68769b268
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2013-12-20 13:30:37 -0500

    Refactor backbone router.
    After some deeper thought, I'm refactoring the router so that it's sole
    purpose is to render a single home view. All other "navigation" will be
    handled using click handlers that update namespaced variables that the
    views all listen to.

--- app/assets/javascripts/routers/home_router.js
+++ app/assets/javascripts/routers/home_router.js
@@ -4,80 +4,12 @@ Nevernote.Routers.Home = Support.SwappingRouter.extend({
   },
 
   routes: {
-    "" : "all",
-    "notebooks/:id" : "showNotebook",
-    "tags/:id" : "showTag",
-    "notes/:id" : "showNote"
+    "" : "home",
   },
 
-  all: function() {
-    var homeRouter = this;
-
-    Nevernote.current_name = "All Notes";
-    Nevernote.notes = new Nevernote.Collections.Notes();
-
-    Nevernote.notes.fetch({
-        success: function() {
-            Nevernote.note = Nevernote.notes.at(0)
-            Nevernote.note.fetch({
-                success: function() {
-                    var view = new Nevernote.Views.Home();
-                    homeRouter.swap(view);
-                }
-            });
-        }
-      });
-  },
-
-  showNotebook: function(id) {
-    var homeRouter = this;
-
-    var notebook = Nevernote.notebooks.get(id);
-    Nevernote.current_name = notebook.get('name');
-
-    notebook.fetch({
-        success: function() {
-            Nevernote.notes = notebook.get('notes');
-            Nevernote.note = Nevernote.notes.at(0);
-            Nevernote.note.fetch({
-                success: function() {
-                    var view = new Nevernote.Views.Home();
-                    homeRouter.swap(view);
-                }
-            });
-        }
-    });
-  },
-
-  showTag: function(id) {
-    var homeRouter = this;
-
-    var tag = Nevernote.tags.get(id)
-    Nevernote.current_name = tag.get('name');
-
-    tag.fetch({
-        success: function() {
-            Nevernote.notes = tag.get('notes');
-            Nevernote.note = Nevernote.notes.at(0);
-            Nevernote.note.fetch({
-                success: function() {
-                    var view = new Nevernote.Views.Home();
-                    homeRouter.swap(view);
-                }
-            });
-        }
-    });
-  },
-
-  showNote: function(id) {
-    var homeRouter = this;
-
-    Nevernote.note = Nevernote.notes.get(id)
-    Nevernote.note.fetch({
-        success: function() {
-            var view = new Nevernote.Views.Home();
-            homeRouter.swap(view);
-        }
-    });
+  home: function() {
+    var view = new Nevernote.Views.Home();
+    this.swap(view);
   }
+
 });
